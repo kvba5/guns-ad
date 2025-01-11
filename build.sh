3d3df1a5e2e8053426567eca649e7c88c7a504fd
kbKuba <kbKuba@proton.me>
2025-01-11 20:09:52 +0000

    Dropped nix cross-compile support

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
-# Builds for both Windows and Linux
+#
+# Nix support for easy compiling is partially dropped
+# due to certain mingw32 packages (required for Windows compiling)
+# are broken.
+#
+# Please send issues/pull requests about them to their respective
+# creators.
+#
+if [ -d /etc/nix/ ]; then
+    echo """
+Cross-compiling with Nix is broken thus is not supported
+You can still build for linux with \"cargo build --release\"
+    """
+    exit
+fi
 
+# Builds for both Windows and Linux
 cargo build --release --target x86_64-pc-windows-gnu \
-                    --target x86_64-unknown-linux-gnu
+                    --target x86_64-unknown-linux-gnu 
